b4889fb4b11972975418ab94b75d9e0026de4b94
Nahue Faluotico <Nahue Faluotico>
2016-08-17 19:12:30 -0300

    Saludos

--- utils.rb
+++ utils.rb
@@ -3,7 +3,9 @@ module Utils
   def self.say_hello_to(name, language)
     if language == "es"
       puts "Hola #{name}!"
+      puts "Todo bien?"
     else
       puts "No entiendo tu idioma."
+      puts "Otro idioma"
   end
 end
